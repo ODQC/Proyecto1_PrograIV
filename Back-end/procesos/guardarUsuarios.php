c99155019c6103e900e182e729e7dd8bffe6ba34
Oscar Quesada <oscaardanielqc@outlook.es>
2021-05-17 22:18:07 -0600

    Update guardarUsuarios.php

--- Back-end/procesos/guardarUsuarios.php
+++ Back-end/procesos/guardarUsuarios.php
@@ -30,7 +30,7 @@ try {
 							alert("No se puedo guardar el usuario correctamente");
 						</script>';
 	}
-	header("Location: ../Proyecto1_PrograIV/Front-end/usuariosF.php");
+	header("Location: Proyecto1_PrograIV/Front-end/usuariosF.php");
 } catch (mysqli_sql_exception $e) {
 	throw $e;
 } catch (Exception $e) {
